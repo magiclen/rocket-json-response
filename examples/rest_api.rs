29336f5d02c1a2f8444162c3352ef97d79fa029e
Magic Len <len@magiclen.org>
2019-06-16 04:48:48 +0800

    update examples

--- examples/rest_api.rs
+++ examples/rest_api.rs
@@ -3,15 +3,18 @@
 #[macro_use]
 extern crate rocket;
 
+#[macro_use]
+extern crate rocket_json_response;
+
 extern crate serde;
 
 #[macro_use]
 extern crate serde_derive;
 
 #[macro_use]
-extern crate rocket_json_response;
+extern crate enum_ordinalize;
 
-use rocket_json_response::{JSONResponse, JSONResponseWithoutData};
+use rocket_json_response::{JSONResponse, JSONResponseWithoutData, JSONResponseCode};
 
 #[derive(Serialize)]
 struct User {
@@ -20,6 +23,16 @@ struct User {
 }
 
 serialize_to_json!(User);
+
+create_ordinalized_enum!(ErrorCode: i32,
+    IncorrectIDFormat = 100,
+);
+
+impl JSONResponseCode for ErrorCode {
+    fn get_code(&self) -> i32 {
+        self.ordinal()
+    }
+}
 
 #[get("/")]
 fn alive() -> JSONResponseWithoutData {
@@ -33,7 +46,7 @@ fn id(id: u32) -> JSONResponse<'static, u32> {
 
 #[get("/<id>", rank = 1)]
 fn id_str(id: String) -> JSONResponse<'static, String> {
-    JSONResponse::err(1, id) // The code "1" means the id is not a u32 value
+    JSONResponse::err(ErrorCode::IncorrectIDFormat, id)
 }
 
 #[get("/user/magiclen")]

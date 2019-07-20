e8aa413e29492516a29786d97db4a466ed0d69f3
Magic Len <len@magiclen.org>
2019-07-20 23:01:34 +0800

    improve Debug

--- src/lib.rs
+++ src/lib.rs
@@ -47,7 +47,7 @@ pub struct JSONResponse<'a, T: ToJSON = JSONGetTextValue<'a>> {
 impl<'a, T: ToJSON> Debug for JSONResponse<'a, T> {
     #[inline]
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        impl_debug_for_struct!(JSONResponse, f, self, (.code, "{}", self.code.get_code()), (.data, "{}", self.data.to_json()));
+        impl_debug_for_struct!(JSONResponse, f, self, let .code = self.code.get_code(), let .data = self.data.to_json());
     }
 }
 
@@ -101,7 +101,7 @@ pub struct JSONResponseWithoutData {
 impl Debug for JSONResponseWithoutData {
     #[inline]
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        impl_debug_for_struct!(JSONResponseWithoutData, f, self, (.code, "{}", self.code.get_code()));
+        impl_debug_for_struct!(JSONResponseWithoutData, f, self, let .code = self.code.get_code());
     }
 }
 

89818b90583fd79ab1af5980341e23b18a1988e7
Magic Len <len@magiclen.org>
2022-05-10 14:07:39 +0800

    run clippy

--- src/to_json.rs
+++ src/to_json.rs
@@ -13,7 +13,7 @@ impl<'a> ToJSON for JSONGetTextValue<'a> {
     }
 }
 
-impl<'a> ToJSON for str {
+impl ToJSON for str {
     #[inline]
     fn to_json(&self) -> String {
         JSONGetTextValue::from_str(self).to_json_string()
@@ -27,84 +27,84 @@ impl<'a> ToJSON for &'a str {
     }
 }
 
-impl<'a> ToJSON for String {
+impl ToJSON for String {
     #[inline]
     fn to_json(&self) -> String {
         JSONGetTextValue::from_str(self).to_json_string()
     }
 }
 
-impl<'a> ToJSON for bool {
+impl ToJSON for bool {
     #[inline]
     fn to_json(&self) -> String {
         JSONGetTextValue::from_bool(*self).to_json_string()
     }
 }
 
-impl<'a> ToJSON for i8 {
+impl ToJSON for i8 {
     #[inline]
     fn to_json(&self) -> String {
         JSONGetTextValue::from_i8(*self).to_json_string()
     }
 }
 
-impl<'a> ToJSON for i16 {
+impl ToJSON for i16 {
     #[inline]
     fn to_json(&self) -> String {
         JSONGetTextValue::from_i16(*self).to_json_string()
     }
 }
 
-impl<'a> ToJSON for i32 {
+impl ToJSON for i32 {
     #[inline]
     fn to_json(&self) -> String {
         JSONGetTextValue::from_i32(*self).to_json_string()
     }
 }
 
-impl<'a> ToJSON for i64 {
+impl ToJSON for i64 {
     #[inline]
     fn to_json(&self) -> String {
         JSONGetTextValue::from_i64(*self).to_json_string()
     }
 }
 
-impl<'a> ToJSON for u8 {
+impl ToJSON for u8 {
     #[inline]
     fn to_json(&self) -> String {
         JSONGetTextValue::from_u8(*self).to_json_string()
     }
 }
 
-impl<'a> ToJSON for u16 {
+impl ToJSON for u16 {
     #[inline]
     fn to_json(&self) -> String {
         JSONGetTextValue::from_u16(*self).to_json_string()
     }
 }
 
-impl<'a> ToJSON for u32 {
+impl ToJSON for u32 {
     #[inline]
     fn to_json(&self) -> String {
         JSONGetTextValue::from_u32(*self).to_json_string()
     }
 }
 
-impl<'a> ToJSON for u64 {
+impl ToJSON for u64 {
     #[inline]
     fn to_json(&self) -> String {
         JSONGetTextValue::from_u64(*self).to_json_string()
     }
 }
 
-impl<'a> ToJSON for f32 {
+impl ToJSON for f32 {
     #[inline]
     fn to_json(&self) -> String {
         JSONGetTextValue::from_f32(*self).to_json_string()
     }
 }
 
-impl<'a> ToJSON for f64 {
+impl ToJSON for f64 {
     #[inline]
     fn to_json(&self) -> String {
         JSONGetTextValue::from_f64(*self).to_json_string()
